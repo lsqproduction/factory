e08bf6d9775c667a90728056a395f69d7f71de74
MicaiahReid <micaiahreid@gmail.com>
2021-06-13 00:17:07 -0400

    change function to deploy

--- migrations/2_script.js
+++ migrations/2_script.js
@@ -1,5 +1,5 @@
-const CC = artifacts.require("CreateCampaigns");
+const CC = artifacts.require("Campaign");
 
 module.exports = function (deployer) {
-  deployer.deploy(CC);
+  deployer.deploy(CC, 1625371200);
 };
